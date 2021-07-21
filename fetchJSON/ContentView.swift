3ccd91a5b8e638f969aa0d0ff3e7892d0b98c121
yokomotoh <yokomotohama@hotmail.com>
2021-07-21 12:45:48 +0200

    Initial Commit

---
+++ fetchJSON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fetchJSON
+//
+//  Created by vincent schmitt on 21/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fetchJSON/fetchJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchJSONApp.swift
+//  fetchJSON
+//
+//  Created by vincent schmitt on 21/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct fetchJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
